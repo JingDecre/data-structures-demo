00784cfde5d3c63be3abaf8c7d8c646d3f334133
decre <l761213825@163.com>
2021-03-03 22:49:37 +0800

    动态规划凑硬币

--- src/main/java/com/decre/data/mathematics/mathematicsdemo/dynamicprogramming/CoinsDemo.java
+++ src/main/java/com/decre/data/mathematics/mathematicsdemo/dynamicprogramming/CoinsDemo.java
@@ -1,5 +1,10 @@
 package com.decre.data.mathematics.mathematicsdemo.dynamicprogramming;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
  * @className: CoinsDemo
  * @description: 凑硬币问题
@@ -10,8 +15,9 @@ public class CoinsDemo {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 5};
-        System.out.println(minCount(arr, 17));
-
+//        System.out.println(minCount1(arr, 17));
+        System.out.println(minCount2(arr, 45));
+        System.out.println(minCount3(arr, 45));
     }
 
     /**
@@ -20,8 +26,8 @@ public class CoinsDemo {
      * @param amount
      * @return
      */
-    private static int minCount(int[] arr, int amount) {
-        return dp(arr, amount);
+    private static int minCount1(int[] arr, int amount) {
+        return dp1(arr, amount);
     }
 
     /**
@@ -31,12 +37,12 @@ public class CoinsDemo {
      * @param amount
      * @return
      */
-    private static int dp(int[] arr, int amount) {
+    private static int dp1(int[] arr, int amount) {
         if (amount == 0) return 0;
         if (amount < 0) return -1;
         int res = Integer.MAX_VALUE;
         for (int coin: arr) {
-            int subMinCount = dp(arr, amount - coin);
+            int subMinCount = dp1(arr, amount - coin);
             if (subMinCount < 0) continue;
             res = Math.min(subMinCount + 1, res);
         }
@@ -44,5 +50,78 @@ public class CoinsDemo {
             return res;
         }
         return -1;
+    }
+
+    /**
+     * 带备忘录递归
+     * @param arr
+     * @param amount
+     * @return
+     */
+    private static int minCount2(int[] arr, int amount) {
+        Map<Integer, Integer> minMap = new HashMap<>();
+        return dp2(minMap, arr, amount);
+    }
+
+    /**
+     * 备忘录递归方法
+     *
+     * @param arr
+     * @param amount
+     * @return
+     */
+    private static int dp2(Map<Integer, Integer> minMap,int[] arr, int amount) {
+        if (minMap.containsKey(amount)) {
+            return minMap.get(amount);
+        }
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+        int res = Integer.MAX_VALUE;
+        for (int coin: arr) {
+            int subMinCount = dp2(minMap, arr, amount - coin);
+            if (subMinCount < 0) continue;
+            res = Math.min(subMinCount + 1, res);
+        }
+        if (Integer.MAX_VALUE != res) {
+            minMap.put(amount, res);
+            return res;
+        }
+        return -1;
+    }
+
+    /**
+     * dp数组解法
+     *
+     * @param arr
+     * @param amount
+     * @return
+     */
+    private static int minCount3(int[] arr, int amount) {
+        return dp3(arr, amount);
+    }
+
+    /**
+     * dp数组核心逻辑
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    private static int dp3(int[] coins, int amount) {
+        int[] minArr = new int[amount + 1];
+        Arrays.fill(minArr, amount + 1);
+        // base case
+        minArr[0] = 0;
+        // 外层 for 循环在遍历所有状态的所有取值
+        for (int i = 0; i < minArr.length; i++) {
+            // 内层 for 循环在求所有选择的最小值
+            for (int coin: coins) {
+                // 子问题无解，跳过
+                if (i- coin < 0) continue;
+
+                minArr[i] = Math.min(minArr[i], 1 + minArr[i - coin]);
+            }
+        }
+        return  (minArr[amount] == amount + 1) ? -1: minArr[amount];
     }
 }
